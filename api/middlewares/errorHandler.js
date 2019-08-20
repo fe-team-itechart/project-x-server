0a5d530081926eb931f0eb9d530653455dd5a9e2
Oleg <oleg.panasyuk2@inbox.ru>
2019-08-20 15:13:32 +0300

    add todo comments to ErrorHandler

--- api/middlewares/errorHandler.js
+++ api/middlewares/errorHandler.js
@@ -1,3 +1,9 @@
+/**
+ * TODO: it needs to convert to class with logging.
+ * @param {*} error: error string
+ * @param {*} options : object of options
+ */
+
 const ErrorHandler = (error, options) => {
   options && options.show && console.log(error);
 };
