55f5773d2886aa2511429f85d2ae803eff050ded
slavasgod <slavasgod@gmail.com>
2019-08-22 12:40:08 +0300

    added error handlers for login

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const controllers = require('../controllers/auth');
 
 router.post('/login', controllers.login);
-
 router.post('/registration', controllers.registration);
 
 module.exports = router;
