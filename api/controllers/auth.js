2c08561de439cdd658c55cb9135b1f4c42508162
slavasgod <slavasgod@gmail.com>
2019-09-06 13:08:49 +0300

    registration fix

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -12,18 +12,18 @@ const login = async (req, res) => {
 };
 
 const socialLogin = async (req, res) => {
-  const response = await services.registration(req.user);
-  res.redirect(process.env.CLIENT_HOST + '?token=' + response);
+  const { token } = await services.socialLogin(req.user);
+  res.redirect(process.env.CLIENT_HOST + '?token=' + token);
 };
 
 const registration = async (req, res) => {
-  const errors = validateAuth(req.body)
-  if(!isEmpty(errors)) {
-    return res.status(400).json(errors)
+  const errors = validateAuth(req.body);
+  if (!isEmpty(errors)) {
+    return res.status(400).json(errors);
   }
 
-  const response = await services.registration(req.body)
-  res.send(response)
+  const response = await services.registration(req.body);
+  res.send(response);
 };
 
 const changePassword = async (req, res) => {
--- api/helpers/jwtHelpers.js
+++ api/helpers/jwtHelpers.js
@@ -4,7 +4,7 @@ const generateToken = ({ id, email }) => {
   const token = jwt.sign({ id, email }, process.env.SECRET, {
     expiresIn: process.env.EXPIRES_IN,
   });
-  return token;
+  return { token };
 };
 
 module.exports = {
