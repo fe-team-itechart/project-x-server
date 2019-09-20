bf4f33d4c01c4836200c3505d5781b4ce4e47d43
melonholy <36301981+melonholy@users.noreply.github.com>
2019-09-20 11:16:51 +0300

    Error update

--- api/services/errorHandlers/userAlreadyExistsError.js
+++ api/services/errorHandlers/userAlreadyExistsError.js
@@ -2,7 +2,7 @@ const ApplicationError = require('./applicationError');
 
 class UserAlreadyExists extends ApplicationError {
   constructor(message) {
-    super(message || 'User already exists.', 400);
+    super(message || 'User already exists.', 400, 'email');
   }
 }
 
