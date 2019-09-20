e9ec209592f2eed2adea258b24726a9829a926ef
melonholy <36301981+melonholy@users.noreply.github.com>
2019-09-20 12:00:33 +0300

    Update errorHandler.js

--- api/middlewares/errorHandler.js
+++ api/middlewares/errorHandler.js
@@ -1,10 +1,10 @@
 const errorHandlerMiddleware = (error, req, res, next) => {
   const { name, status, message, type } = error;
   res.status(error.status).json({
-    name: name,
-    status: status,
-    message: message,
-    type: type,
+    name,
+    status,
+    message,
+    type,
   });
 };
 
