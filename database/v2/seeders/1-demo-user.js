fc6c3c2c36bc1e6936bd6ff06f5be88864707fe4
Oleg <oleg.panasyuk2@inbox.ru>
2019-10-24 14:22:24 +0300

    Implement order, fix password, mock public-profile

---
+++ database/v2/seeders/1-demo-user.js
@@ -0,0 +1,65 @@
+const hashHelpers = require('../../../api/helpers/hashHelpers');
+
+async function createMockUser(index) {
+  const data = await hashHelpers.createHash('Password123456789!');
+  const obj = {
+    id: index + 1,
+    userName: `John Doe`,
+    email: `demo${index}@demo.com`,
+    password: data,
+    role: 'User',
+    locale: 'ru',
+    resetPasswordToken: null,
+  };
+  return obj;
+}
+
+function createMockProfile(index) {
+  const profile = {
+    id: index + 1,
+  };
+  return profile;
+}
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const users = await Promise.all(
+      Array(10)
+        .fill(1)
+        .map((el, index) => {
+          return createMockUser(index).then(user => user);
+        })
+    );
+
+    const profiles = Array(10)
+      .fill(1)
+      .map((el, index) => {
+        return createMockProfile(index);
+      });
+
+    return Promise.all([
+      queryInterface.bulkInsert('users', users, {}),
+      queryInterface.bulkInsert('public_profiles', profiles, {}),
+    ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    const whereUsers = {
+      email: {
+        [Op.regexp]: 'demo[0-9]{0,}@demo.com',
+      },
+    };
+
+    const whereProfiles = {
+      id: {
+        [Op.and]: [{ [Op.gte]: 1 }, { [Op.lte]: 10 }],
+      },
+    };
+
+    return Promise.all([
+      queryInterface.bulkDelete('users', whereUsers, {}),
+      queryInterface.bulkDelete('public_profiles', whereProfiles, {}),
+    ]);
+  },
+};
